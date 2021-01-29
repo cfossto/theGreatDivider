a7237db44d924d633386eb78815fa74f1884ff92
cfossto <70951673+cfossto@users.noreply.github.com>
2021-01-29 17:50:41 +0100

    first commit

---
+++ theGreatDivider.py
@@ -0,0 +1,38 @@
+# Welcome to theGreatDivider.py!
+
+# This app reads a CSV-file-path and devide the content according to your specifications
+# The first row is always a column name
+
+import csv
+import random
+
+
+file_path = input("\nWrite the path to the file here: \n")
+
+newList = []
+
+with open(file_path) as f:
+    d = csv.reader(f)
+    for i in d:
+        newList.append(i)
+
+
+
+formated_list = []
+for member in newList:
+    for row in member:
+        row = row.replace(";","")
+        row = row.replace(",","")
+        formated_list.append(row)
+
+
+random.shuffle(formated_list)
+
+member_num = int(input("\nHow many people in every group? "))
+final_list = [formated_list[x:x+member_num] for x in range(0,len(formated_list),member_num)]
+
+
+for groups in final_list:
+    print("\nGroup: {}".format(final_list.index(groups)+1))
+    for member in groups:
+        print(member.capitalize())
